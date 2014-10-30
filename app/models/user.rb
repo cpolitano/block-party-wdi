97a88c082c282cd67138dd4fe9fa78e9af29d4cc
cpolitano <cpolitano8@gmail.com>
2014-10-30 16:25:17 -0400

    add additional trigger words to array

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   has_many :blocks
+  has_many :words
+  has_many :tweets
+
+  # Words associated with online harassment
+  @trigger_words = ["rape","murder","nigger","slut","whore","bitch","cunt","kill","die","testword"]
 
   def self.create_with_omniauth(auth)
     create! do |user|
@@ -10,14 +15,25 @@ class User < ActiveRecord::Base
     end
   end
 
-  # Compares text of a mention to trigger words array. Returns a username if user's tweet text includes a trigger word.
+  # Add additional words to trigger words array
+  def get_my_words
+    my_words = Word.where(user_id: self.id)
+
+    my_words.each do |word|
+      @trigger_words << my_words
+    end
+    return @trigger_words
+  end
+
+  # Compare text of a mention to trigger words array. Returns a username if user's tweet text includes a trigger word.
   def determine_blocks(twitter, mentions)
-    # Words associated with online harassment
-    trigger_words = ["rape","murder","nigger","slut","whore","bitch","cunt","kill","die","testword"]
+    
+    @trigger_words = get_my_words
     users_to_block = []
+
     mentions.each do |mention|
       mention.text.split(" ").each do |word|
-        if trigger_words.include?(word) && twitter.friendship?(self, mention.user.screen_name) == false
+        if @trigger_words.include?(word) && twitter.friendship?(self, mention.user.screen_name) == false
             users_to_block << mention.user.screen_name
         end
       end
@@ -28,9 +44,10 @@ class User < ActiveRecord::Base
 
   # Compares text of a mention to trigger words array. Returns a new tweet object for any tweet containing a trigger word.
   def determine_triggers(twitter, mentions)
-    # Words associated with online harassment
-    trigger_words = ["rape","murder","nigger","slut","whore","bitch","cunt","kill","die","testword"]
+    
+    @trigger_words = get_my_words
     trigger_tweets = []
+    
     mentions.each do |mention|
       mention.text.split(" ").each do |word|
         if trigger_words.include?(word) && twitter.friendship?(self, mention.user.screen_name) == false
