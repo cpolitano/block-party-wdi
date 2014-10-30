8482602646c2138c3657de3ecb2ec5a84d2277a8
cpolitano <cpolitano8@gmail.com>
2014-10-30 16:03:32 -0400

    refactored ajax calls

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,22 +1,35 @@
 $(document).ready(function(){
   
   // AJAX call to execute destroy function in blocks controller
-  $("li.unblock").on("click", function(){
-    var self = this;
-    id = $(this).attr("id");
-    button = $(this).children();
+  deleteItem: function(type, item) {
+    id = $(item).attr("id");
+    button = $(item).children();
     button.css({"background-color": "#B531E8"});
-    button.text("unblocked!");
-
+    button.text("deleted!");
     $.ajax({
-      url: "blocks/" + id, 
+      url: type + "/" + id, 
       type: "DELETE",
       dataType: "json",
       success: function() {
         console.log('success!');
-        $(self).delay(10000).fadeOut(10000).remove();
+        $(item).delay(10000).fadeOut(10000).remove();
       }
     });
+  }
+
+  $("li.unblock").on("click", function(){
+    var self = this;
+    deleteItem("blocks",self);
+  })
+
+  $("li.delete-tweet").on("click", function(){
+    var self = this;
+    deleteItem("tweets", self);
+  })
+
+  $("li.delete-word").on("click", function(){
+    var self = this;
+    deleteItem("words", self);
   })
   
 });
